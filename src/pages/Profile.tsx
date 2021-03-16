281c2766469047c9083785a9eb9a10c5eeba14a4
SMART_PC\YIJIA <zyj82073693@gmail.com>
2021-03-16 20:03:16 +0800

    Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -15,9 +15,12 @@ import {female, male, personCircle} from "ionicons/icons";
 import { useHistory } from "react-router-dom";
 import { IonItem, IonLabel, IonInput, IonButton, IonIcon, IonAlert,IonCheckbox,IonText } from '@ionic/react';
 import {IonRouteInner} from "@ionic/react-router/dist/types/ReactRouter/IonRouteInner";
-import {personAddOutline,maleFemaleOutline,calendarNumberOutline,callOutline,mailOutline,lockClosedOutline} from "ionicons/icons";
-
-
+import {personAddOutline} from "ionicons/icons";
+import {calendarNumberOutline} from "ionicons/icons";
+import {callOutline} from "ionicons/icons";
+import {mailOutline} from "ionicons/icons";
+import {lockClosedOutline} from "ionicons/icons";
+import {maleFemaleOutline} from "ionicons/icons";
 
 function validateEmail(email: string) {
     const re = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
@@ -25,24 +28,15 @@ function validateEmail(email: string) {
 }
 
 
-const Register: React.FC = () => {
+// @ts-ignore
+const Profile: React.FC = () => {
   const history = useHistory();
   const [email, setEmail] = useState<string>("eve.holt@reqres.in");
   const [password, setPassword] = useState<string>("cityslicka");
   const [iserror, setIserror] = useState<boolean>(false);
   const [message, setMessage] = useState<string>("");
 
-  const handleLogin = () => {
-    if (!email) {
-        setMessage("Please enter a valid email");
-        setIserror(true);
-        return;
-    }
-    if (validateEmail(email) === false) {
-        setMessage("Your email is invalid");
-        setIserror(true);
-        return;
-    }
+
 
     if (!password || password.length < 6) {
         setMessage("Please enter your password");
@@ -174,37 +168,7 @@ const Register: React.FC = () => {
             </IonCol>
 
 
-            <IonCol>
-            <IonItem>
-                <IonIcon
-                    style={{ fontSize: "20px", color: "#0040ff" }}
-                    icon={lockClosedOutline}
-                />
-              <IonLabel position="floating"> Password</IonLabel>
-              <IonInput
-                type="password"
-                value={password}
-                onIonChange={(e) => setPassword(e.detail.value!)}
-                >
-              </IonInput>
-            </IonItem>
-            </IonCol>
 
-                <IonCol>
-                    <IonItem>
-                        <IonIcon
-                            style={{ fontSize: "20px", color: "#0040ff" }}
-                            icon={lockClosedOutline}
-                        />
-                        <IonLabel position="floating"> Confirm Password</IonLabel>
-                        <IonInput
-                            type="password"
-                            value={password}
-                            onIonChange={(e) => setPassword(e.detail.value!)}
-                        >
-                        </IonInput>
-                    </IonItem>
-                </IonCol>
 
 
             <IonCol>
@@ -220,6 +184,6 @@ const Register: React.FC = () => {
       </IonContent>
     </IonPage>
   );
-};
 
-export default Register;
+
+export default Profile;
