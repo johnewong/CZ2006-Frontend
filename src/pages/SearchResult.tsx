4092171c414674d6fcc39a404fb1a094385f6933
ShirleyMaX <shirleystar0525@gmail.com>
2021-03-17 23:38:25 +0800

    mx update

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,11 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonContent, IonHeader, IonInfiniteScrollContent, IonPage, IonSlide, IonTitle, IonToolbar,IonVirtualScroll } from '@ionic/react';
 import React, { useState } from 'react';
 import axios from "axios";
 import { IonGrid, IonRow, IonCol } from '@ionic/react';
 import { personCircle } from "ionicons/icons";
 import { lockClosed } from "ionicons/icons";
 import { useHistory } from "react-router-dom";
-import { IonItem, IonLabel, IonInput, IonButton, IonIcon, IonAlert,IonImg } from '@ionic/react';
+import { IonItem, IonLabel, IonInput, IonButton, IonIcon, IonAlert,IonImg, IonSegment,IonSegmentButton, IonSlides, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/react';
 
 function validateEmail(email: string) {
   
@@ -20,6 +20,14 @@ const SearchResult: React.FC = () => {
   const handleLogin = () => {
  
   };
+  const slideOpts = {
+    //https://swiperjs.com/swiper-api
+    initialSlide: 0,
+    speed: 300,
+    direction: "horizontal"
+    
+  };
+  
 
   return (
     <IonPage>
@@ -33,16 +41,67 @@ const SearchResult: React.FC = () => {
                   <b>Jurong West</b>
               </IonLabel>
             </IonCol>        
-          </IonRow>
+          </IonRow>  
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="ion-padding ion-text-center">
         <IonGrid>
-        <IonRow>
-          <IonCol>
-          </IonCol>
-        </IonRow>
-        
+          <IonRow>
+              <IonCol>
+              <IonSlides pager ={true} options={slideOpts}>
+                <IonSlide>
+                  <IonSegment>
+                    <IonSegmentButton style = {{height: "60px"}}>Date 1</IonSegmentButton>
+                    <IonSegmentButton style = {{height: "60px"}}>Date 2</IonSegmentButton>
+                    <IonSegmentButton style = {{height: "60px"}}>Date 3</IonSegmentButton>
+                    <IonSegmentButton style = {{height: "60px"}}>Date 4</IonSegmentButton>
+                  </IonSegment>
+                </IonSlide>
+              </IonSlides>
+              </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle class = "ion-text-left">Clinic Name1</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent class = "ion-text-left">
+                  Location: 501 Jurong West Street 51, # 01-283
+                  Hong Kah Point, Singapore 640591
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle class = "ion-text-left">Clinic Name2</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent class = "ion-text-left">
+                  Location: 501 Jurong West Street 51, # 01-283
+                  Hong Kah Point, Singapore 640591
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle class = "ion-text-left">Clinic Name3</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent class = "ion-text-left">
+                  Location: 501 Jurong West Street 51, # 01-283
+                  Hong Kah Point, Singapore 640591
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
         </IonGrid>
       </IonContent>
     </IonPage>
