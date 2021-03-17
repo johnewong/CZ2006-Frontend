50a8e3df7528ed67694061b28a5b52cb0f3fbed1
ShirleyMaX <shirleystar0525@gmail.com>
2021-03-17 23:49:39 +0800

    mx update

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -41,25 +41,41 @@ const SearchResult: React.FC = () => {
                   <b>Jurong West</b>
               </IonLabel>
             </IonCol>        
-          </IonRow>  
-        </IonToolbar>
+          </IonRow>
+          </IonToolbar>
+          </IonHeader>
+          <IonHeader>   
+            <IonToolbar>
+              <IonRow>
+                <IonCol>
+                  <IonSegment>
+                    <IonSegmentButton style = {{height: "60px"}}>
+                      Select Date
+                    </IonSegmentButton>      
+                  </IonSegment>
+                </IonCol>
+              </IonRow>
+            <IonRow>
+
+                <IonCol>
+                  <IonSlides pager ={true} options={slideOpts}>
+                    <IonSlide>
+                      <IonSegment>
+                        <IonSegmentButton style = {{height: "60px"}}>Date 1</IonSegmentButton>
+                        <IonSegmentButton style = {{height: "60px"}}>Date 2</IonSegmentButton>
+                        <IonSegmentButton style = {{height: "60px"}}>Date 3</IonSegmentButton>
+                        <IonSegmentButton style = {{height: "60px"}}>Date 4</IonSegmentButton>
+                      </IonSegment>
+                    </IonSlide>
+                  </IonSlides>
+                </IonCol>
+            </IonRow>  
+          
+          </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="ion-padding ion-text-center">
         <IonGrid>
-          <IonRow>
-              <IonCol>
-              <IonSlides pager ={true} options={slideOpts}>
-                <IonSlide>
-                  <IonSegment>
-                    <IonSegmentButton style = {{height: "60px"}}>Date 1</IonSegmentButton>
-                    <IonSegmentButton style = {{height: "60px"}}>Date 2</IonSegmentButton>
-                    <IonSegmentButton style = {{height: "60px"}}>Date 3</IonSegmentButton>
-                    <IonSegmentButton style = {{height: "60px"}}>Date 4</IonSegmentButton>
-                  </IonSegment>
-                </IonSlide>
-              </IonSlides>
-              </IonCol>
-          </IonRow>
+          
           <IonRow>
             <IonCol>
               <IonCard>
@@ -92,7 +108,7 @@ const SearchResult: React.FC = () => {
             <IonCol>
               <IonCard>
                 <IonCardHeader>
-                  <IonCardTitle class = "ion-text-left">Clinic Name3</IonCardTitle>
+                  <IonCardTitle class = "ion-text-left">Clinic Name</IonCardTitle>
                 </IonCardHeader>
 
                 <IonCardContent class = "ion-text-left">
