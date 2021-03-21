77a3d7d60ecd24677aad1b6a4ed3664d522c35e0
SMART_PC\YIJIA <zyj82073693@gmail.com>
2021-03-21 18:06:23 +0800

    Setting

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -6,15 +6,25 @@ import {
     IonList,
     IonInput,
     IonToggle,
-   IonItemSliding,
-    IonIcon, IonItem, IonLabel, IonRadio, IonCheckbox, IonCardTitle, IonCardContent,  IonButton
+    IonItemSliding,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonRadio,
+    IonCheckbox,
+    IonCardTitle,
+    IonCardContent,
+    IonButton,
+    IonToolbar,
+    IonTitle,
+    IonPage
 
 } from "@ionic/react";
 import React, { useState } from "react";
 import axios from "axios";
 import { IonGrid, IonRow, IonCol } from "@ionic/react";
 import {
-    settingsOutline, idCardOutline
+    settingsOutline,saveOutline,informationCircleOutline, idCardOutline,peopleOutline,notificationsOutline,languageOutline
 } from "ionicons/icons";
 
 const Setting: React.FC = () => {
@@ -22,28 +32,42 @@ const Setting: React.FC = () => {
 
     return (
         <IonContent>
+            <IonHeader>
 
-            <IonItem>
-            <IonIcon style={{ fontSize: "20px", color: "#0040ff" }} icon={settingsOutline}/>
-            <IonLabel>Setting</IonLabel>
-        </IonItem>
+                    <IonItem>
+                    <IonIcon style={{ fontSize: "20px", color: "#0040ff" }} icon={settingsOutline}/>
+                    <IonTitle>Setting</IonTitle>
+                    </IonItem>
+
+            </IonHeader>
 
             <IonList>
 
                 <IonItem>
-                    <IonLabel>Account</IonLabel>
+                    <IonIcon style={{ fontSize: "20px", color: "#0040ff" }} icon={peopleOutline}/>
+                    <IonGrid>
+                    <IonLabel>Account</IonLabel></IonGrid>
+
                 </IonItem>
                 <IonItem>
-                    <IonLabel>Notification</IonLabel>
+                    <IonIcon style={{ fontSize: "20px", color: "#0040ff" }} icon={notificationsOutline}/>
+                    <IonGrid>
+                    <IonLabel>Notification</IonLabel></IonGrid>
                 </IonItem>
                 <IonItem>
-                    <IonLabel>Language</IonLabel>
+                    <IonIcon style={{ fontSize: "20px", color: "#0040ff" }} icon={languageOutline}/>
+                    <IonGrid>
+                    <IonLabel>Language</IonLabel></IonGrid>
                 </IonItem>
                 <IonItem>
-                    <IonLabel>Privacy & Security</IonLabel>
+                    <IonIcon style={{ fontSize: "20px", color: "#0040ff" }} icon={saveOutline}/>
+                    <IonGrid>
+                    <IonLabel>Privacy & Security</IonLabel></IonGrid>
                 </IonItem>
                 <IonItem>
-                    <IonLabel>About</IonLabel>
+                    <IonIcon style={{ fontSize: "20px", color: "#0040ff" }} icon={informationCircleOutline}/>
+                    <IonGrid>
+                    <IonLabel>About</IonLabel></IonGrid>
                 </IonItem>
             </IonList>
 
