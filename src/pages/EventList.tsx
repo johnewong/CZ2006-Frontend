74c9630ec2355c6fa7e9726f9f232d6a93715a26
yfweng <johnewong95@gmail.com>
2021-03-27 20:52:16 +0800

    add eventlist button func

--- src/pages/EventList.tsx
+++ src/pages/EventList.tsx
@@ -13,7 +13,8 @@ import {
   IonCardContent,
   IonButton,
 } from "@ionic/react";
-import React, { useState } from "react";
+import { Collapse } from "react-collapse";
+import React, { useCallback, useState } from "react";
 import axios from "axios";
 import { IonGrid, IonRow, IonCol } from "@ionic/react";
 import { pin, idCardOutline } from "ionicons/icons";
@@ -30,9 +31,9 @@ import "./EventList.css";
 // const [isActive, setActive] = useState<boolean>(false);
 
 let dataString = "";
-let appointment_data = [
+const appointment_data = [
   {
-    isActive:false,
+    isActive: false,
     appointmentDate: "4 Feb 2020",
     appointmentStartTime: "10:30 AM",
     appointmentEndTime: "11:00 AM",
@@ -49,7 +50,7 @@ let appointment_data = [
     },
   },
   {
-    isActive:false,
+    isActive: false,
     appointmentDate: "5 Feb 2020",
     appointmentStartTime: "10:30 AM",
     appointmentEndTime: "11:00 AM",
@@ -67,119 +68,19 @@ let appointment_data = [
   },
 ];
 
-//const [activeItem, setActiveItem] = React.useState(1);
-
-function handleExpand(item:any){
-  item.isActive = true;
-  alert(item.isActive);
-}
-
-function EventDetail(item:any){
-  //alert("displayDetail"+item.isActive);
-  return (
-    <div>
-       <IonRow>
-            <IonCol>
-              <IonLabel>Vet: </IonLabel>
-              <IonLabel>{item.veter.veterName}</IonLabel>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonLabel>Treatment/Service: </IonLabel>
-              <IonLabel>{item.treatment.treatmentName}</IonLabel>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonLabel>Address: </IonLabel>
-              <IonLabel>{item.vet.vetAddress}</IonLabel>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonLabel>Tel: </IonLabel>
-              <IonLabel>{item.vet.tel}</IonLabel>
-            </IonCol>
-          </IonRow>
-    </div>
-  );
-}
-
-function displayEvent(item: any, index: any) {
-  
-  return (
-   <IonCard>
-      <IonCardContent class="ion-text-left">
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonLabel>
-                <div className="card-title">Appointment</div>
-              </IonLabel>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol size="5">
-              <IonIcon
-                style={{ fontSize: "25px", color: "#0040ff" }}
-                icon={calendarNumberOutline}
-              />
-              <IonLabel> </IonLabel>
-              <IonLabel>{item.appointmentDate}</IonLabel>
-            </IonCol>
-            <IonCol size="5">
-              <IonIcon
-                style={{ fontSize: "25px", color: "#0040ff" }}
-                icon={timerOutline}
-              />
-              <IonLabel> </IonLabel>
-              <IonLabel>{item.appointmentStartTime}</IonLabel>
-            </IonCol>
-            <IonCol>
-              <IonIcon
-                style={{ fontSize: "25px", color: "#0040ff" }}
-                icon={chevronDown}
-                onClick={()=>  handleExpand(index)}
-              />
-            </IonCol>
-          </IonRow>
-                
-           {EventDetail(item)}
-
-          {/* <IonRow>
-            <IonCol>
-              <IonLabel>Vet: </IonLabel>
-              <IonLabel>{item.veter.veterName}</IonLabel>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonLabel>Treatment/Service: </IonLabel>
-              <IonLabel>{item.treatment.treatmentName}</IonLabel>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonLabel>Address: </IonLabel>
-              <IonLabel>{item.vet.vetAddress}</IonLabel>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonLabel>Tel: </IonLabel>
-              <IonLabel>{item.vet.tel}</IonLabel>
-            </IonCol>
-          </IonRow> */}
-          
-        </IonGrid>
-      </IonCardContent>
-    </IonCard>
-  );
-}
-
 const EventList: React.FC = () => {
-  //getData();
+  const [collapseNumber, setCollapseNumber] = useState(0);
+  function onClick (index:number) {
+    //useCallback(()=>setCollapseNumber(index), [collapseNumber]);
+    console.log("click!", index);
+    console.log("collapseNumber!", collapseNumber);
+    if(index ==  collapseNumber){
+      console.log("toggle");
+      setCollapseNumber(100);
+    }
+    setCollapseNumber(index);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -188,7 +89,77 @@ const EventList: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        {appointment_data.map((item, index) => displayEvent(item, index))}
+        {appointment_data.map((item, index) => {
+          // console.log("collapseNumber", collapseNumber);
+          // console.log("index date", index, item.appointmentDate);
+          return (
+            <IonCard>
+              <IonCardContent class="ion-text-left">
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonLabel>
+                        <div className="card-title">Appointment</div>
+                      </IonLabel>
+                    </IonCol>
+                  </IonRow>
+                  <IonRow>
+                    <IonCol size="5">
+                      <IonIcon
+                        style={{ fontSize: "25px", color: "#0040ff" }}
+                        icon={calendarNumberOutline}
+                      />
+                      <IonLabel> </IonLabel>
+                      <IonLabel>{item.appointmentDate}</IonLabel>
+                    </IonCol>
+                    <IonCol size="5">
+                      <IonIcon
+                        style={{ fontSize: "25px", color: "#0040ff" }}
+                        icon={timerOutline}
+                      />
+                      <IonLabel> </IonLabel>
+                      <IonLabel>{item.appointmentStartTime}</IonLabel>
+                    </IonCol>
+                    <IonCol>
+                      <IonIcon
+                        style={{ fontSize: "25px", color: "#0040ff" }}
+                        icon={chevronDown}
+                        onClick={(e) => onClick(index)}
+                      />
+                    </IonCol>
+                  </IonRow>
+                  {/* <Collapse isOpened={isButtonCollapseOpen}> */}
+                  <Collapse isOpened={index == collapseNumber}>
+                    <IonRow>
+                      <IonCol>
+                        <IonLabel>Vet: </IonLabel>
+                        <IonLabel>{item.veter.veterName}</IonLabel>
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>
+                        <IonLabel>Treatment/Service: </IonLabel>
+                        <IonLabel>{item.treatment.treatmentName}</IonLabel>
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>
+                        <IonLabel>Address: </IonLabel>
+                        <IonLabel>{item.vet.vetAddress}</IonLabel>
+                      </IonCol>
+                    </IonRow>
+                    <IonRow>
+                      <IonCol>
+                        <IonLabel>Tel: </IonLabel>
+                        <IonLabel>{item.vet.tel}</IonLabel>
+                      </IonCol>
+                    </IonRow>
+                  </Collapse>
+                </IonGrid>
+              </IonCardContent>
+            </IonCard>
+          );
+        })}
       </IonContent>
     </IonPage>
   );
